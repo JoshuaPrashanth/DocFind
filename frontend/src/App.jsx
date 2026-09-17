20d4f6912557f6cc2d806afdc4b7c7a2fccaace6
JoshuaPrashanth <joshuaprashanth.in@gmail.com>
2026-09-17 22:55:03 +0530

    Add upload section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
-import UploadSection from "./UploadSection";
+import UploadSection from "./components/UploadSection";
 function App() {
   return (
     <div className="container">
       <h1>DocFind</h1>
       <p>PDF Document Search</p>
-      <UploadSection></UploadSection>
+      <UploadSection />
     </div>
   );
 }
