17c740b0c13892dbaff44750e218c4a02aa54523
JoshuaPrashanth <joshuaprashanth.in@gmail.com>
2026-09-19 23:46:31 +0530

    Add Search Results Dummy

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,16 @@
-
 import UploadSection from "./components/UploadSection";
 import SearchBar from "./components/SearchBar.jsx";
+import SearchResults from "./components/SearchResults.jsx";
 
 function App() {
   return (
     <div className="container">
       <h1>DocFind</h1>
       <p>PDF Document Search</p>
-    
+
       <UploadSection />
       <SearchBar />
+      <SearchResults />
     </div>
   );
 }
