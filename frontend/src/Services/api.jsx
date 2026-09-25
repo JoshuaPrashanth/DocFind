b8ab6bd675a597b1de35f6cd38ffc23c87100469
Manjuprasad M <manjuprasadm04@gmail.com>
2026-09-25 22:49:56 +0530

    files are stored in envelop formData and uploaded to backend through api

--- frontend/src/Services/api.jsx
+++ frontend/src/Services/api.jsx
@@ -9,7 +9,22 @@ async function checkBackendStatus(){
 }
 
 export async function uploadPDFs(files){
-  
+  const formData=new FormData();
+
+  for(let i=0;i<files.length;i++){
+    formData.append("pdfs",files[i]);
+  }
+
+  const response=await fetch("http://localhost:3000/api/upload",{
+    method:"POST",
+    body:"formData",
+  })
+
+  if(!response.ok){
+    throw new Error("upload failed");
+  }
+
+  return await response.json();
 }
 
 export default checkBackendStatus;
